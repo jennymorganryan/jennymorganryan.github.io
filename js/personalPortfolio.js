6609cae7aec2bdaf912abefef2d86a2dbd22b6d3
jennymorganryan <86384143+jennymorganryan@users.noreply.github.com>
2024-02-23 14:59:41 -0600

    intro done

--- js/personalPortfolio.js
+++ js/personalPortfolio.js
@@ -1,7 +1,18 @@
+const clickableBar = document.querySelector('#clickable-bar');
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
+const navbarLinks = document.querySelectorAll('.navbar__links');
 
-menu.addEventListener('click', function() {
+
+clickableBar.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
+});
+
+// Close menu and links on navbar__links click
+navbarLinks.forEach(link => {
+    link.addEventListener('click', function() {
+        menu.classList.remove('is-active');
+        menuLinks.classList.remove('active');
+    });
 });
